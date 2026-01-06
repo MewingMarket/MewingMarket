1545a3d2f6168ca1eb899517ee180e9b5c9bb0dc
MewingMarket <MewingMarket@outlook.it>
2026-01-07 00:33:09 +0100

    server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import path from "path";
-
+app.disable("x-powered-by");
 const app = express();
 
 // =========================
