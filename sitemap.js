edf2ce16fe841ac7aeb3a26beaa94ba5e7b50558
MewingMarket <MewingMarket@outlook.it>
2026-01-12 22:38:08 +0100

    sitemap.js

---
+++ sitemap.js
@@ -0,0 +1,13 @@
+// modules/sitemap.js
+
+// File placeholder per futura sitemap dinamica.
+// Attualmente non contiene logica, come richiesto.
+
+function generateSitemap() {
+  // Da implementare in futuro
+  return "";
+}
+
+module.exports = {
+  generateSitemap
+};
