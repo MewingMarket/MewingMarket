7bb99d9d74710fe052e55a555562c50f18c2ff02
MewingMarket <MewingMarket@outlook.it>
2026-01-18 15:20:30 +0100

    brevoSubscrive.js

---
+++ modules/brevoSubscrive.js
@@ -0,0 +1,15 @@
+const axios = require("axios");
+
+async function iscriviEmail(email) {
+  const apiKey = process.env.BREVO_API_KEY;
+
+  await axios.post(
+    "https://api.brevo.com/v3/contacts",
+    { email, listIds: [8] },
+    { headers: { "api-key": apiKey, "Content-Type": "application/json" } }
+  );
+
+  return { status: "ok" };
+}
+
+module.exports = { iscriviEmail };
