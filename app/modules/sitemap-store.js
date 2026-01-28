9263f967c25795e984858de8d61ea0f57bf18904
MewingMarket <MewingMarket@outlook.it>
2026-01-28 22:26:29 +0100

    sitemap-store.js

---
+++ app/modules/sitemap-store.js
@@ -0,0 +1,26 @@
+// modules/sitemap-store.js
+
+const { getProducts } = require("./airtable");
+
+function generateStoreSitemap() {
+  const products = getProducts();
+
+  let xml = `<?xml version="1.0" encoding="UTF-8"?>\n`;
+  xml += `<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n`;
+
+  products.forEach(p => {
+    if (!p.linkPayhip) return;
+
+    xml += `
+  <url>
+    <loc>${p.linkPayhip}</loc>
+    <changefreq>weekly</changefreq>
+    <priority>0.8</priority>
+  </url>`;
+  });
+
+  xml += `\n</urlset>`;
+  return xml;
+}
+
+module.exports = { generateStoreSitemap };
