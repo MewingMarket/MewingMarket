a88e0e27f8ff51c70fa9401b139c56d200ac56c5
MewingMarket <MewingMarket@outlook.it>
2026-02-13 19:53:34 +0100

    server.cjs

--- app/server.cjs
+++ app/server.cjs
@@ -203,8 +203,7 @@ app.use(express.static(path.join(ROOT, "app", "public")));
 app.use(cors({ origin: true, credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
-app.use('/api/payhip', require('./routes/payhip'));
-
+app.use('/api/payhip', require('./routes/payhip.cjs'));
 /* =========================================================
    USER STATE + COOKIE UID
 ========================================================= */
