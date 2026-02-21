a8f4f46035cc9436a492ef46b3ac40f4a0bd9c16
MewingMarket <MewingMarket@outlook.it>
2026-02-21 02:59:36 +0100

    airtable.cjs

--- app/server/services/airtable.cjs
+++ app/server/services/airtable.cjs
@@ -1,3 +1,8 @@
+// =========================================================
+// File: app/server/services/airtable.cjs
+// Wrapper Airtable universale per tutte le API
+// =========================================================
+
 const Airtable = require("airtable");
 
 const base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY })
