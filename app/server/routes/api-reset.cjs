1758388ce16b1b5973f19137c2b272ac11a18938
MewingMarket <MewingMarket@outlook.it>
2026-02-23 15:52:55 +0100

    api-reset.cjs
    Updated airtable import to use .cjs extension.

--- app/server/routes/api-reset.cjs
+++ app/server/routes/api-reset.cjs
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const crypto = require("crypto");
-const { airtable } = require("../services/airtable");
+const crypto = require("crypto");const { airtable } = require("../services/airtable.cjs");
 
 // Hash password
 function hashPassword(pwd) {
