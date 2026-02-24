6cddef5b8fc8823ef3646348ba7184cf16bd596a
MewingMarket <MewingMarket@outlook.it>
2026-02-24 22:02:22 +0100

    api-paypal-create.cjs

--- app/server/routes/api-paypal-create.cjs
+++ app/server/routes/api-paypal-create.cjs
@@ -5,7 +5,7 @@
 // =========================================================
 
 const express = require("express");
-const Airtable = require("airtable").default;
+const Airtable = require("../lib/airtable-wrapper.cjs");
 const fetch = require("node-fetch");
 const authUser = require("../middleware/auth-user.cjs");
 
