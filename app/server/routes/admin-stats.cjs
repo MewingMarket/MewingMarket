3a0b16ea84613356ad36c67e362e34cee75e2766
MewingMarket <MewingMarket@outlook.it>
2026-02-25 21:31:31 +0100

    admin-stats.cjs

--- app/server/routes/admin-stats.cjs
+++ app/server/routes/admin-stats.cjs
@@ -2,7 +2,7 @@
 
 const express = require("express");
 const router = express.Router();
-const authAdmin = require("../middleware/authAdmin.cjs");
+const authAdmin = require("../middleware/auth-admin.cjs"); // PATCH QUI
 
 // ❌ Vecchio require (non esiste più)
 // const base = require("../lib/airtable.cjs");
