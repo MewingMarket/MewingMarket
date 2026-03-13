c6f213ac753853b7f4f4abdee3d366845df43a81
MewingMarket <MewingMarket@outlook.it>
2026-03-13 21:42:00 +0100

    utenti.sql

---
+++ app/server/db/utenti.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS utenti (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  email TEXT UNIQUE NOT NULL,
+  password_hash TEXT NOT NULL,
+  ruolo TEXT DEFAULT 'user',
+  token TEXT,
+  reset_password_token TEXT,
+  reset_email_token TEXT,
+  created_at TEXT DEFAULT CURRENT_TIMESTAMP
+);
