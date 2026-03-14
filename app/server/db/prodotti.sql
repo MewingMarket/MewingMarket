7b7654143d85c42f80a8a4287e277a4a359ae3ce
MewingMarket <MewingMarket@outlook.it>
2026-03-14 19:06:53 +0100

    prodotti.sql

---
+++ app/server/db/prodotti.sql
@@ -0,0 +1,18 @@
+CREATE TABLE IF NOT EXISTS prodotti (
+  titolo_breve TEXT NOT NULL,
+  slug TEXT UNIQUE NOT NULL,
+  prezzo_cent INTEGER NOT NULL,
+  descrizione_breve TEXT,
+  descrizione_lunga TEXT,
+
+  youtube_url TEXT,
+  youtube_title TEXT,
+  youtube_thumbnail TEXT,
+  youtube_video_id TEXT,
+
+  immagine_url TEXT,
+  file_consegna_url TEXT,
+
+  created_at TEXT DEFAULT CURRENT_TIMESTAMP,
+  updated_at TEXT DEFAULT CURRENT_TIMESTAMP
+);
