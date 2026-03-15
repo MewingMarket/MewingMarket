abac29cc6894b450bbba52109086ba8f83d7bd85
MewingMarket <MewingMarket@outlook.it>
2026-03-16 00:01:56 +0100

    prodotti.sql

--- app/server/db/prodotti.sql
+++ app/server/db/prodotti.sql
@@ -1,12 +1,15 @@
-CREATE TABLE IF NOT EXISTS prodotti (
+DROP TABLE IF EXISTS prodotti;
+
+CREATE TABLE prodotti (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   titolo TEXT NOT NULL,
+  titolo_breve TEXT,
   descrizione_lunga TEXT,
   descrizione_breve TEXT,
-  prezzo REAL NOT NULL,
-  immagine TEXT,
-  fileProdotto TEXT,
+  prezzo_cent INTEGER NOT NULL,
   categoria TEXT,
+  immagine_url TEXT,
+  file_consegna_url TEXT,
   youtube_url TEXT,
   youtube_video_id TEXT,
   youtube_title TEXT,
